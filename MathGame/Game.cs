97d8f02a82e87bd2d6adcde9c3ffe8a4aff61887
ChrisFelt <christopher.f.felt@gmail.com>
2025-06-04 02:46:06 -0600

    added initial implementation for Game

--- MathGame/Game.cs
+++ MathGame/Game.cs
@@ -1,4 +1,6 @@
 using MathGame;
+using System;
+using System.Timers;
 
 class Game
 {
@@ -9,14 +11,63 @@ class Game
     // time elapsed, read-only
     // List of operations INCLUDING "random", no getter or setter
     // List of questions asked and answers given, read-only
+    private string _difficultyMode;
+    private string _operationChoice;
+    private System.Timers.Timer _timer;
+    private String[] _operations = { "+", "-", "*", "/" };
+    private String[] _difficulties = { "too easy", "easy", "normal" };
+    public int TimeElapsed { get; private set; }
+    public List<string> QuestionHistory { get; private set; }
 
     // extra properties that may be used:
     // count of questions asked, read-only
     // count of correct answers given, read-only
+    public int TotalQuestions { get; private set; }
+    public int CorrectAnswers { get; private set; }
+
+    // constructor
+    public Game(string difficulty, string operation)
+    {
+        _difficultyMode = difficulty;
+        _operationChoice = operation;
+        StartTimer();
+    }
 
     // methods:
     // 1. internal start and stop timer methods
-    // 2. random operand method randomly generates an operand uses difficulty mode to determine size of number
+    private void StartTimer()
+    {
+        // one second interval timer
+        _timer = new System.Timers.Timer(1000);
+        _timer.Elapsed += OnTimedEvent;
+        _timer.AutoReset = true;  // enables repeated events
+        _timer.Enabled = true;  // starts timer
+    }
+    public void StopTimer()
+    {
+        _timer.Stop();
+        _timer.Dispose();
+    }
+    private void OnTimedEvent(object? source, ElapsedEventArgs e)
+    {
+        TimeElapsed++;
+    }
+
+    
+    // 2. random number method uses difficulty mode to determine size of number
+    private int GenerateNum(int start, int end)
+    {
+        Random rand = new Random();
+
+        // to comply with division specifications:
+        // in static constructor: generate all factors of every number between 0 and 100 and store in an array of lists
+        // generate a random number between 0 and 100 for dividend
+        // find a random factor of that number from the array of lists and use it as the divisor
+        // 1 must always be an option
+        // in case of a 0 for dividend, simply generate a random number between 1 and 100
+        // 0 must never be an option for the divisor
+    }
+
     // 3. check answer method takes operands, operator, and answer. 
     // Checks the answer given and updates questions asked and correct answers given properties
     // 4. play method uses (2) to generate random numbers, gets operator, and prints question, then gets user input and calls (3)
