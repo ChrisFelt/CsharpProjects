7937ab42f872706fcf0e28b73da716403e31c7f0
ChrisFelt <christopher.f.felt@gmail.com>
2025-06-04 02:43:13 -0600

    added initial GameHistory implementation

--- MathGame/GameHistory.cs
+++ MathGame/GameHistory.cs
@@ -4,9 +4,23 @@ class GameHistory
 {
     // properties:
     // empty List that will contain Game objects, no getters or setters
+    private List<Game> _gameList;
+
+    public GameHistory()
+    {
+        _gameList = new List<Game>();
+    }
 
     // methods:
     // 1. add game, takes a Game object and returns nothing. Adds game object to game list
+    public void AddGame(Game game)
+    {
+        _gameList.Add(game);
+    }
     // 2. print all, loops through every Game object in the game list and calls its print history method
     // Returns no games played message if empty
+    public void ShowGames()
+    {
+
+    }
 }
