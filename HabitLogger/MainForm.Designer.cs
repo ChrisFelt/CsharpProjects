a58fe28ba5998f9342214d37d18f906c1eea415c
ChrisFelt <christopher.f.felt@gmail.com>
2025-08-31 02:18:49 -0600

    Add project files.

---
+++ HabitLogger/MainForm.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace HabitLogger
+{
+    partial class main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pnlLogin = new System.Windows.Forms.Panel();
+            this.lblNewUser = new System.Windows.Forms.Label();
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblWelcome = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.pnlMain = new System.Windows.Forms.Panel();
+            this.underConstructionLabel = new System.Windows.Forms.Label();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.pnlLogin.SuspendLayout();
+            this.pnlMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlLogin
+            // 
+            this.pnlLogin.Controls.Add(this.lblNewUser);
+            this.pnlLogin.Controls.Add(this.lblUserName);
+            this.pnlLogin.Controls.Add(this.btnExit);
+            this.pnlLogin.Controls.Add(this.btnStart);
+            this.pnlLogin.Controls.Add(this.lblWelcome);
+            this.pnlLogin.Controls.Add(this.txtUserName);
+            this.pnlLogin.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlLogin.Location = new System.Drawing.Point(0, 0);
+            this.pnlLogin.Name = "pnlLogin";
+            this.pnlLogin.Size = new System.Drawing.Size(800, 450);
+            this.pnlLogin.TabIndex = 0;
+            // 
+            // lblNewUser
+            // 
+            this.lblNewUser.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblNewUser.AutoSize = true;
+            this.lblNewUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNewUser.Location = new System.Drawing.Point(278, 315);
+            this.lblNewUser.Name = "lblNewUser";
+            this.lblNewUser.Size = new System.Drawing.Size(244, 16);
+            this.lblNewUser.TabIndex = 6;
+            this.lblNewUser.Text = "New User? Click here to create a profile!";
+            this.lblNewUser.Click += new System.EventHandler(this.lblNewUser_Click);
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblUserName.AutoSize = true;
+            this.lblUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUserName.Location = new System.Drawing.Point(285, 179);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(93, 20);
+            this.lblUserName.TabIndex = 5;
+            this.lblUserName.Text = "User Name:";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnExit.Location = new System.Drawing.Point(443, 244);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(76, 37);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnStart.Location = new System.Drawing.Point(360, 244);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(76, 37);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblWelcome
+            // 
+            this.lblWelcome.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblWelcome.AutoSize = true;
+            this.lblWelcome.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWelcome.Location = new System.Drawing.Point(225, 88);
+            this.lblWelcome.Name = "lblWelcome";
+            this.lblWelcome.Size = new System.Drawing.Size(350, 31);
+            this.lblWelcome.TabIndex = 2;
+            this.lblWelcome.Text = "Welcome to Habit Logger!";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtUserName.Location = new System.Drawing.Point(282, 201);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(236, 26);
+            this.txtUserName.TabIndex = 1;
+            // 
+            // pnlMain
+            // 
+            this.pnlMain.Controls.Add(this.underConstructionLabel);
+            this.pnlMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlMain.Location = new System.Drawing.Point(0, 0);
+            this.pnlMain.Name = "pnlMain";
+            this.pnlMain.Size = new System.Drawing.Size(800, 450);
+            this.pnlMain.TabIndex = 0;
+            // 
+            // underConstructionLabel
+            // 
+            this.underConstructionLabel.AutoSize = true;
+            this.underConstructionLabel.Location = new System.Drawing.Point(281, 187);
+            this.underConstructionLabel.Name = "underConstructionLabel";
+            this.underConstructionLabel.Size = new System.Drawing.Size(227, 20);
+            this.underConstructionLabel.TabIndex = 0;
+            this.underConstructionLabel.Text = "Main Panel under construction.";
+            // 
+            // main
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pnlLogin);
+            this.Controls.Add(this.pnlMain);
+            this.Name = "main";
+            this.Text = "Habit Logger";
+            this.pnlLogin.ResumeLayout(false);
+            this.pnlLogin.PerformLayout();
+            this.pnlMain.ResumeLayout(false);
+            this.pnlMain.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlLogin;
+        private System.Windows.Forms.Panel pnlMain;
+        private System.Windows.Forms.Label lblWelcome;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Label underConstructionLabel;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblUserName;
+        private System.Windows.Forms.Label lblNewUser;
+        private System.Windows.Forms.BindingSource bindingSource1;
+    }
+}
+
---
+++ HabitLogger/MainForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HabitLogger
+{
+    public partial class main : Form
+    {
+        public main()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblNewUser_Click(object sender, EventArgs e)
+        {
+            // open NewUserForm window
+            NewUserForm newUser = new NewUserForm();
+            newUser.Show();
+        }
+    }
+}
