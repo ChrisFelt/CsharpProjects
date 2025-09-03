c126f9bc19f7a0a42bf2f2a77392784ff99f51ae
ChrisFelt <christopher.f.felt@gmail.com>
2025-09-03 03:49:26 -0600

    Initial DML, CRUD operations for Users (needs testing)

---
+++ HabitLogger/database/DML.sql
@@ -0,0 +1,53 @@
+-- Data Manipulation Language for Habit Logger
+
+-- -----------------------------------------------------
+-- Users Data Manipulation Queries
+-- -----------------------------------------------------
+
+-- CREATE
+-- add a new User
+-- currently only contains 2 attributes which may be expanded at a later date, see 
+-- associated DbOutline.md
+INSERT INTO Users (userName)
+VALUES (userNameInput);  -- userNameInput variable value determined by user input
+
+-- READ
+-- get all Users
+SELECT userID		AS 'User ID',
+	   userName		AS 'User Name'
+FROM Users;
+
+-- UPDATE
+-- allow user to modify their user name
+-- first statement used to populate current user profile info
+SELECT *
+FROM Users
+WHERE userID = userIDInput;
+-- update current user
+UPDATE Users
+SET Users.userName		= userNameInput
+WHERE Users.userID = userIDInput;  -- this value automatically supplied by the app
+
+-- DELETE
+-- first, delete all records from Dates that would no longer have any relationships 
+-- with Habits after the Habit is cascade deleted when User is deleted
+DELETE
+FROM Dates
+WHERE dateID = (SELECT d.dateID,
+					   h.habitID,
+					   h.userID
+				FROM Dates AS d
+				INNER JOIN Habits_has_Dates AS hd
+					ON d.dateID = hd.dateID
+				INNER JOIN Habits AS h
+					ON hd.habitID = h.habitIP
+				WHERE h.userID = userIDInput  -- all habits and dates for the user
+					AND (SELECT COUNT(dateID)
+						 FROM Habits_has_Dates
+						 WHERE dateID = d.dateID) <= 1);  -- limit to dates with 1 or less relationships for that date
+
+-- remove the user and all associated habits - automatic due to ON DELETE CASCADE 
+-- foreign key constraint in Habits table
+DELETE
+FROM Users
+WHERE userID = userIDInput;
