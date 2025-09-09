6fc02d3a1a26c5ae98445c01f2ea3f8b86b98fb5
ChrisFelt <christopher.f.felt@gmail.com>
2025-09-09 02:56:14 -0600

    Added basic db connection and create/read methods for Users

--- HabitLogger/DbController.cs
+++ HabitLogger/DbController.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Data.SQLite;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 /*
 Database Controller class.
@@ -8,8 +14,84 @@ namespace HabitLogger
 {
 	public class DbController
 	{
+		public SQLiteConnection conn;
+
+		// default constructor
 		public DbController()
 		{
+			// TODO: move db to project directory
+            DbConnect(@"../../database/HabitLoggerDb.db");
+            // TODO: only execute this method if the database is empty
+            RunDdlFromFile();
 		}
-	}
+
+        public void DbConnect(string path)
+		{
+			conn = new SQLiteConnection($"Data Source={path}; Version=3; New=True;Compress=True");
+			//attempt to open db connection
+			try
+			{
+				conn.Open();
+			}
+			catch (Exception ex)
+			{
+				// notify user and exit the program (Application.Exit() does not work here)
+                MessageBox.Show($"{ex.Message}", "DB Connect Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Environment.Exit(1);  
+			}
+		}
+
+		// create tables and run sample inserts from DDL.sql
+		public void RunDdlFromFile()
+		{
+            SQLiteCommand cmd = conn.CreateCommand();
+
+            // get embedded resource DDL.sql 
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            string resourceName = assembly.GetManifestResourceNames().Single(str => str.EndsWith("DDL.sql"));
+
+			using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+			using (StreamReader reader = new StreamReader(stream))
+			{
+				cmd.CommandText = reader.ReadToEnd();
+			}
+			
+            try
+            {
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex.Message}", "DDL Read Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        public void CreateUser(string userName)
+		{
+			SQLiteCommand cmd = conn.CreateCommand();
+			cmd.CommandText = $"INSERT INTO Users (userName) VALUES ('{userName}');";
+			try
+			{
+                cmd.ExecuteNonQuery();
+            }
+			catch (Exception ex)
+			{
+                MessageBox.Show($"{ex.Message}", "Username Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+		}
+
+        public void ReadUser()
+        {
+			// TODO: BROKEN. fix read.GetString(0) 
+			SQLiteCommand cmd = conn.CreateCommand();
+			cmd.CommandText = "SELECT userID AS 'User ID', userName AS 'User Name' FROM Users;";
+			SQLiteDataReader read = cmd.ExecuteReader();
+			while (read.Read())
+			{
+				Console.WriteLine(read.GetString(0));
+			}
+        }
+    }
 }
