97da5d11234975ab8185a5d3dd342c1d096596b3
ChrisFelt <christopher.f.felt@gmail.com>
2025-09-11 03:09:34 -0600

    added basic login/logout logic

--- HabitLogger/MainForm.cs
+++ HabitLogger/MainForm.cs
@@ -13,20 +13,50 @@ namespace HabitLogger
     public partial class main : Form
     {
         DbController sqliteDb = new DbController();
+        int curUserID = 0;  // user not logged in
         public main()
         {
             InitializeComponent();
             CenterToScreen();
+            // show only login panel when app is started
+            pnlLogin.Show();
+            pnlMain.Hide();
         }
 
-        private void btnStart_Click(object sender, EventArgs e)
+        private void btnLogin_Click(object sender, EventArgs e)
         {
+            // validate username input
+            string inputTxt = txtUserName.Text;
+            if (string.IsNullOrWhiteSpace(inputTxt))
+            {
+                MessageBox.Show("Invalid Username.\nPlease enter one or more characters.", "Username Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                // attempt to login
+                inputTxt = inputTxt.Trim(' ');
+                curUserID = sqliteDb.ReadUser(inputTxt);
 
+                if (curUserID == 0)
+                {
+                    // login failed, notify user
+                    MessageBox.Show($"Username: '{inputTxt}' does not exist.\nPlease try again.", "Login failed.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    // swap to main panel if login succeeds
+                    pnlLogin.Hide();
+                    pnlMain.Show();
+
+                    // clear txtUserName for next login
+                    txtUserName.Clear();
+                }
+            }
         }
 
         private void btnExit_Click(object sender, EventArgs e)
         {
-
+            Close();
         }
 
         private void lblNewUser_Click(object sender, EventArgs e)
@@ -36,5 +66,12 @@ namespace HabitLogger
             newUser.Show();
         }
 
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            // logout user and swap back to login panel
+            pnlLogin.Show();
+            pnlMain.Hide();
+            curUserID = 0;
+        }
     }
 }
