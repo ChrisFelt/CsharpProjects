38350c9011f31c95b3e7eda313ef4b36a038bf42
ChrisFelt <christopher.f.felt@gmail.com>
2025-09-17 23:46:40 -0600

    added cancel button event

--- HabitLogger/HabitLogger/AddHabitForm.Designer.cs
+++ HabitLogger/HabitLogger/AddHabitForm.Designer.cs
@@ -80,6 +80,7 @@
             this.btnCancel.TabIndex = 9;
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
             // lblAddHabit
             // 
