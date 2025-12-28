486b33f42d609f4d7783f341f478bc45330ded15
ChrisFelt <christopher.f.felt@gmail.com>
2025-12-27 22:29:04 -0700

    removed type element from the tuple

--- HabitLogger/HabitLogger/DataGridViewHistory.cs
+++ HabitLogger/HabitLogger/DataGridViewHistory.cs
@@ -11,18 +11,16 @@ namespace HabitLogger
     public class DataGridViewHistory
     {
         // controls the activity history for a DataGridView 
-        // history stacks contain tuple that begins with identifier "cell" or "row"  TODO: probably don't need identifier, delete?
         // C# stack data structure documentation: https://learn.microsoft.com/en-us/dotnet/api/system.collections.stack?view=net-10.0
-        private Stack<(string type, int row, string note, int quantity, int habitHasDateID)> undoHistory = new Stack<(string type, int row, string note, int quantity, int habitHasDateID)>();
-        private Stack<(string type, int row, string note, int quantity, int habitHasDateID)> redoHistory = new Stack<(string type, int row, string note, int quantity, int habitHasDateID)>();
+        private Stack<(int row, string note, int quantity, int habitHasDateID)> undoHistory = new Stack<(int row, string note, int quantity, int habitHasDateID)>();
+        private Stack<(int row, string note, int quantity, int habitHasDateID)> redoHistory = new Stack<(int row, string note, int quantity, int habitHasDateID)>();
         
         public DataGridViewHistory() { }  // empty constructor
 
-        // TODO: add public methods RedoCell, RedoRow, UndoCell, and UndoRow. Make Redo and Undo private. Add "cell" and "row" as class variables
-        public (string type, int row, string note, int quantity, int habitHasDateID) Redo((string type, int row, string note, int quantity, int habitHasDateID) values)
+        public (int row, string note, int quantity, int habitHasDateID) Redo((int row, string note, int quantity, int habitHasDateID) values)
         {
             // pop top value off redoHistory
-            (string type, int row, string note, int quantity, int habitHasDateID) returnValue = redoHistory.Pop();
+            (int row, string note, int quantity, int habitHasDateID) returnValue = redoHistory.Pop();
             // find its destination cell(s) in the dt
             // push current values of those cell(s) into undoHistory
             undoHistory.Push(values);
@@ -32,10 +30,10 @@ namespace HabitLogger
             // write changes to database
         }
 
-        public (string type, int row, string note, int quantity, int habitHasDateID) Undo((string type, int row, string note, int quantity, int habitHasDateID) values)
+        public (int row, string note, int quantity, int habitHasDateID) Undo((int row, string note, int quantity, int habitHasDateID) values)
         {
             // pop top value off undoHistory
-            (string type, int row, string note, int quantity, int habitHasDateID) returnValue = undoHistory.Pop();
+            (int row, string note, int quantity, int habitHasDateID) returnValue = undoHistory.Pop();
             // find its destination cell(s) in the dt
             // push current values of those cell(s) into redoHistory
             redoHistory.Push(values);
@@ -44,7 +42,7 @@ namespace HabitLogger
             // write changes to database
         }
 
-        public void Commit((string type, int row, string note, int quantity, int habitHasDateID) values)
+        public void Commit((int row, string note, int quantity, int habitHasDateID) values)
         {
             // clear redoHistory stack and push values on top of undoHistory
             redoHistory.Clear();
