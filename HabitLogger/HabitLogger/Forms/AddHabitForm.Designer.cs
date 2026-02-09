c9e5f94ede449cbebf717bf898912c52d940c610
ChrisFelt <christopher.f.felt@gmail.com>
2026-02-08 21:30:33 -0700

    changed combobox drop down style to DropDownList to prevent user from editing contents

--- HabitLogger/HabitLogger/Forms/AddHabitForm.Designer.cs
+++ HabitLogger/HabitLogger/Forms/AddHabitForm.Designer.cs
@@ -100,6 +100,7 @@
             // comboBoxHabitName
             // 
             this.comboBoxHabitName.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.comboBoxHabitName.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBoxHabitName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBoxHabitName.FormattingEnabled = true;
             this.comboBoxHabitName.Location = new System.Drawing.Point(36, 34);
