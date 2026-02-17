46711f40da438a5ad03ea17dabec9d5ed6e87e9f
ChrisFelt <54368648+ChrisFelt@users.noreply.github.com>
2026-02-17 15:19:35 -0700

    Delete Chatter directory

--- Chatter/GlobalXmlns.cs
+++
@@ -1,2 +0,0 @@
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Chatter")]
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Chatter.Pages")]
